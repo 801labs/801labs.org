db4c781081477964c6fe719e04d0bbe82406544e
Admiral Potato <admiral@nuclearpixel.com>
2021-11-03 01:44:17 -0600

    Update social footer

--- components/SocialLinks.js
+++ components/SocialLinks.js
@@ -2,11 +2,8 @@ import React from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {
     faDiscord,
-    faFacebookSquare,
     faGithub,
-    faInstagram,
     faMeetup,
-    faSlack,
     faTwitterSquare,
     faYoutube
 } from "@fortawesome/free-brands-svg-icons";
@@ -15,17 +12,7 @@ const SocialLinks = (props) => {
     return <ul className={"flex flex-wrap " + props.ulClassNames}>
         <li className={props.liClassNames}>
             <a className={props.aClassNames}
-               href="https://www.meetup.com/801labs/"
-               rel="noopener"
-               target="_blank"
-               title="Meetup">
-                <span className="sr-only">Meetup</span>
-                <FontAwesomeIcon icon={faMeetup} size={props.size}/>
-            </a>
-        </li>
-        <li className={props.liClassNames}>
-            <a className={props.aClassNames}
-               href="https://discord.com/Un3gXVs"
+               href="https://discord.gg/7pBUdwv9Gr"
                rel="noopener"
                target="_blank"
                title="Discord">
@@ -45,6 +32,16 @@ const SocialLinks = (props) => {
         </li>
         <li className={props.liClassNames}>
             <a className={props.aClassNames}
+               href="https://www.meetup.com/801labs/"
+               rel="noopener"
+               target="_blank"
+               title="Meetup">
+                <span className="sr-only">Meetup</span>
+                <FontAwesomeIcon icon={faMeetup} size={props.size}/>
+            </a>
+        </li>
+        <li className={props.liClassNames}>
+            <a className={props.aClassNames}
                href="https://www.youtube.com/c/801LabsSaltLakeCity"
                rel="noopener"
                target="_blank"
@@ -55,32 +52,12 @@ const SocialLinks = (props) => {
         </li>
         <li className={props.liClassNames}>
             <a className={props.aClassNames}
-               href="https://discord.gg/7pBUdwv9Gr"
-               rel="noopener"
-               target="_blank"
-               title="Discord">
-                <span className="sr-only">Discord</span>
-                <FontAwesomeIcon icon={faDiscord} size={props.size}/>
-            </a>
-        </li>
-        <li className={props.liClassNames}>
-            <a className={props.aClassNames}
                href="https://github.com/801labs/"
                rel="noopener"
                target="_blank"
                title="GitHub">
                 <span className="sr-only">GitHub</span>
                 <FontAwesomeIcon icon={faGithub} size={props.size}/>
-            </a>
-        </li>
-        <li className={props.liClassNames}>
-            <a className={props.aClassNames}
-               href="https://fb.me/801labs"
-               rel="noopener"
-               target="_blank"
-               title="Facebook">
-                <span className="sr-only">Facebook</span>
-                <FontAwesomeIcon icon={faFacebookSquare} size={props.size}/>
             </a>
         </li>
     </ul>
