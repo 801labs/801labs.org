3f7c10fee707df355c24690e44f794ed23a02592
Yagoth <celestial.ameba@gmail.com>
2021-11-07 22:26:00 -0700

    Added COVID rules to location and hours, removed Tuesday hours

--- pages/get-involved/location-and-hours.js
+++ pages/get-involved/location-and-hours.js
@@ -25,9 +25,17 @@ const LocationAndHours = () => (
             </div>
             <div className="lg:w-3/5">
                 <div className="mb-5">
+                    <p className="font-bold" >
+                    COVID-19 Restrictions
+                    </p>
+                    <p style={{color: 'red'}} >
+                        A vaccination or negative COVID-19 test (within the last 7 days) is required to enter the hackerspace. We are currently on the honor system, however private verification of vaccine or negative test status will be required in the future.  Effective: November 5th 2021
+                    </p>
+                </div>    
+                <div className="mb-5">
                     <p className="font-bold">353 East 200 South Suite #201, Salt Lake City, UT 84111</p>
                     <p>
-                        Tuesdays and Thursdays from 5:00 PM to 10:00 PM
+                        Thursdays from 5:00 PM to 10:00 PM and other days by announcement.
                     </p>
                 </div>
                 <div className="mb-5">
